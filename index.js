803fe36b10b3e594b2f4577f0437a123868561df
Jacek Jan Pietal <jacekjanpietal@pop-os.localdomain>
2021-08-30 02:35:32 +0200

    feat: add mime types

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const AWS = require("aws-sdk/dist/aws-sdk-react-native");
+const AWS = require("aws-sdk");
+const mime = require("mime-types");
+const chalk = require("chalk");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = function (s3Path, bucketName) {
+const dryRun = Array.from(process.argv).join(" ").includes("--dry");
+const folder = process.argv[2];
+const bucket = process.argv[3];
+
+if (folder && bucket) {
+  uploadDir(folder, bucket);
+} else {
+  if (!folder) {
+    console.error("Missing foler name");
+  }
+
+  if (!bucket) {
+    console.error("Missing bucket name");
+  }
+
+  console.info("Usage: upload2s3 folder bucket");
+}
+
+function uploadDir(s3Path, bucketName) {
+  const dirName = path.basename(s3Path);
   const s3 = new AWS.S3();
 
   function walkSync(currentDirPath, callback) {
@@ -17,36 +38,36 @@ const uploadDir = function (s3Path, bucketName) {
     });
   }
 
-  walkSync(s3Path, function (filePath, stat) {
-    let bucketPath = filePath.substring(s3Path.length + 1);
-    let params = {
+  walkSync(s3Path, function (filePath) {
+    const bucketPath = `${dirName}${filePath.split(dirName)[1]}`;
+    const params = {
       Bucket: bucketName,
       Key: bucketPath,
       Body: fs.readFileSync(filePath),
     };
-    s3.putObject(params, function (err, data) {
+
+    if (bucketPath.endsWith("/")) {
+      return;
+    }
+
+    params.ContentType = mime.lookup(bucketPath);
+
+    const uploadLog = `${chalk.blueBright(bucketPath)} as ${chalk.whiteBright(
+      params.ContentType
+    )}`;
+
+    if (dryRun) {
+      console.info(`Dry upload: ${mimeLog}`);
+
+      return;
+    }
+
+    s3.putObject(params, function (err) {
       if (err) {
-        console.log(err);
+        console.log(err.message);
       } else {
-        console.log(
-          "Successfully uploaded " + bucketPath + " to " + bucketName
-        );
+        console.info(`Uploaded: ${uploadLog}`);
       }
     });
   });
-};
-
-const folder = process.argv[2];
-const bucket = process.argv[3];
-
-if (folder && bucket) {
-  uploadDir(folder, bucket);
-} else {
-  if (!folder) {
-    console.error("Missing foler name");
-  }
-  if (!bucket) {
-    console.error("Missing bucket name");
-  }
-  console.info("Usage: upload2s3 folder bucket");
 }
